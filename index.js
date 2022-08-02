9ae343b0ccf7f84fb292620be5e53b047a6be432
Djabrail Magamadov <magamadov.ru095@gmail.com>
2022-08-02 16:42:22 +0300

    Создал основу для бэка

---
+++ index.js
@@ -0,0 +1,25 @@
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+// require('dotenv').config();
+const path = require("path")
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static('images'));
+// app.use('/images', express.static(__dirname + '/images'));
+
+app.use(require('./routes/index'));
+
+mongoose.connect(process.env.MONGO_SERVER, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
+
+app.listen(process.env.PORT, () => {
+   console.log(process.env.MONGO_SERVER)
+   console.log('Connected...')
+})
