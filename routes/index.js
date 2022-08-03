8acdb7d92508cc53b984038ca04bcb968f6c1fe8
Paranoid <magomaevd99@gmail.com>
2022-08-03 16:14:11 +0300

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const { Router } = require("express");
 const router = Router();
 
 
-router.use(require('./carts.route'))
+//router.use(require('./carts.route'))
 router.use(require('./category.route'))
-router.use(require('./drugs.route'))
+//router.use(require('./drugs.route'))
 router.use(require('./recomendations.route'))
 router.use(require("./reviews.route"));
 router.use(require('./services.route'))
