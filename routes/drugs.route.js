6b1188902e79f9f709c3758bb634df8c6cd5cdbc
Djabrail Magamadov <magamadov.ru095@gmail.com>
2022-08-08 14:39:53 +0300

    Изменения

--- routes/drugs.route.js
+++ routes/drugs.route.js
@@ -1,10 +1,11 @@
 const Router = require("express");
 const { drugController } = require("../controllers/drugs.controller");
+const fileMiddleware = require("../middlewares/fileMiddleware");
 
 const router = Router();
 
 
-router.post("/drug", drugController.postDrug)
+router.post("/drug",fileMiddleware.single("image"), drugController.postDrug)
 router.get('/drug', drugController.getDrug)
 router.delete('/drug/:id', drugController.delDrug)
 
